64dc1f46710919bb2c1348da9e4c557e113ed795
Hamzaseed <hs602100@gmail.com>
2025-08-26 16:34:51 +0500

    Update package name to 'scalix' and add homepage URL; format main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,10 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+
+
+
+<BrowserRouter        >
     <App />
   </BrowserRouter>,
 )
