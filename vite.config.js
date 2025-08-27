155b6888d573152ee3403e193af8d5a62b1c9900
Hamzaseed <hs602100@gmail.com>
2025-08-27 16:11:48 +0500

    viteconfig.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Project-Scalix/'
+  base: '/Project-Scalix/',   // 👈 must match your repo
 })
