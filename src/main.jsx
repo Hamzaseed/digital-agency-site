6775e7bfbdc133c5ce4fdde11cd713d2a47562cc
Hamzaseed <hs602100@gmail.com>
2025-08-27 13:54:27 +0500

    index.css , vite.config ,main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,9 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-
-<BrowserRouter        >
+<StrictMode>
+  <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+</StrictMode>
 )
