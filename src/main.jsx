761b8b34a686cdd4b343aa688fe9e36bdd24778c
Hamzaseed <hs602100@gmail.com>
2025-10-05 20:17:34 +0500

    Save work before redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter basename='/Project-Scalix'  >
+  <BrowserRouter basename='/digital-agency-site'  >
     <App />
   </BrowserRouter>
 
