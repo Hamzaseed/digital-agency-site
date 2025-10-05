8be216d0a03c8060019c2d830d0b4aebe359fb6c
Hamzaseed <hs602100@gmail.com>
2025-10-05 19:57:20 +0500

    Remove FireBase

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// Replace "Project-Scalix" with your actual repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/Project-Scalix/',   // 👈 must match your repo
-})
+  base: "/Project-Scalix/",
+});
